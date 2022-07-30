f26eb407bd4973c76606224c00668f77a30a11a3
Guilherme Correa <correacgui@gmail.com>
2022-07-30 14:31:39 -0300

    Added breadthFirstTraversal -> Graph search

--- graphSearch.js
+++ graphSearch.js
@@ -1,3 +1,5 @@
+const Queue = require('./helpers/Queue')
+
 const depthFirstTraversal = (start, visitedVertices = [start]) => {
   console.log(start.data)
 
@@ -10,4 +12,24 @@ const depthFirstTraversal = (start, visitedVertices = [start]) => {
   })
 }
 
-module.exports = { depthFirstTraversal }
+const breadthFirstTraversal = start => {
+  const visitedVertices = [start]
+  const visitQueue = new Queue()
+  visitQueue.enqueue(start)
+
+  while (!visitQueue.isEmpty()) {
+    const current = visitQueue.dequeue()
+    console.log(current.data)
+    current.edges.forEach(edge => {
+      const neighbor = edge.end
+
+      if (!visitedVertices.includes(neighbor)) {
+        visitedVertices.push(neighbor)
+        visitQueue.enqueue(neighbor)
+      }
+    })
+
+  }
+}
+
+module.exports = { depthFirstTraversal, breadthFirstTraversal }
